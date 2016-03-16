4ebbc65a815afc0045fb8d821273afc7fe2dde15
Matheus Landim Cruz <teteus_landim@hotmail.com>
2016-03-16 18:15:50 -0300

    Inclusão do ex004

---
+++ ex004/ex004.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+int x, y;
+
+scanf ("%d", &x);
+scanf ("%d", &y);
+printf ("O maior numero digitado foi");
+if (x > y) {
+
+  printf ("%d", x);
+  } else {
+
+  printf ("%d", y);
+}
+return 0;
+}
